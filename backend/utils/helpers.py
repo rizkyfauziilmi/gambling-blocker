6a4fc2bf8e593c83b054d61419c58ee3dc14208d
Rizky Fauzi Ilmi <rizkyfauziilmi@gmail.com>
2026-06-01 22:52:29 +0700

    remove fragment and query from url

--- backend/utils/helpers.py
+++ backend/utils/helpers.py
@@ -41,6 +41,8 @@ def resolve_ips(hostname: str) -> list[str]:
 
 
 def clean_url(url: str) -> str:
+    parsed = urlparse(url)
+    url = parsed.netloc + parsed.path
     url = unquote(url).lower()
     url = re.sub(r"https?:\/\/", "", url)
     url = re.sub(r"[-_/]", " ", url)
@@ -48,4 +50,5 @@ def clean_url(url: str) -> str:
     url = re.sub(r"(\d)([a-z])", r"\1 \2", url)
     url = re.sub(r"([a-z])(\d)", r"\1 \2", url)
     url = re.sub(r"\s+", " ", url).strip()
+
     return url
